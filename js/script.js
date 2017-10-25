e29f6269a0aae6cb1809c991605c92bca28ca32c
rocketkevin <kevinchen91200@gmail.com>
2017-10-25 22:21:11 +0000

    Organized

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-//Unit 1 Portfolio with Firebase
-// Use Firebase to store the images links and info for your projects. 
-// Use Jquery to dynamically display the information from firebse. 
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+// window.onclick = function(event) {
+//   if (!event.target.matches('#aboutme')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// };
+ // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBJ5fFFAJeV3MMTKD0MMd_ycz3U1alOeRY",
+  authDomain: "my-profile-dea20.firebaseapp.com",
+  databaseURL: "https://my-profile-dea20.firebaseio.com",
+  projectId: "my-profile-dea20",
+  storageBucket: "my-profile-dea20.appspot.com",
+  messagingSenderId: "1050589712437"
+};
+firebase.initializeApp(config);
+    
+var database = firebase.database();
 
+var Name = database.ref('firstName');
+
+Name.on('value', function(snapshot) {
+      var name = snapshot.val();
+      $('#name').html(name);
+      // console.log("Hi");
+});
