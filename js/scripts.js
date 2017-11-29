0448106579e9a03da2ddfb9906ddf5a9ae9219b7
rocketkevin <kevinchen91200@gmail.com>
2017-11-29 21:37:55 +0000

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// -----------firebase----------------------------
 var config = {
   apiKey: "AIzaSyBJ5fFFAJeV3MMTKD0MMd_ycz3U1alOeRY",
   authDomain: "my-profile-dea20.firebaseapp.com",
@@ -14,15 +15,11 @@ var info = database.ref('Info');
 var projects = database.ref('Projects');
 var images = database.ref('Images');
 var contacts = database.ref('Contacts');
-
+// ----------------------------------------------
 info.on('value', function(snapshot) {
       var value = snapshot.val();
       $('#name').html(value.firstName);
       $('#name').append(' ' + value.lastName);
-      $('#highschool').html('High School: ' + value.highSchool);
-      $('#address').html('Mailing Addrees: ' + value.Address);
-      $('#birth').html('Birth: ' + value.Birth);
-      $('#gender').html('Gender: ' + value.Gender);
 });
 
 projects.on('value', function(snapshot) {
@@ -38,29 +35,52 @@ images.on('value', function(snapshot) {
         $('#panelThree').append('<p>' + key + '</p>');
       }
 });
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    };
+// button click--------------------------------
+function addClickHandlersToButtons() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            
+            // $('.panel').append('<div id = ' + 'panelOne' + '><p id='+'highschool'+'></p><p id='+'address'+'></p><p id='+'birth'+'></p><p id='+'gender'+'></p></div>')
+            
+            info.on('value', function(snapshot) {
+                var value = snapshot.val();
+                $('#highschool').html('High School: ' + value.highSchool);
+                $('#address').html('Mailing Addrees: ' + value.Address);
+                $('#birth').html('Birth: ' + value.Birth);
+                $('#gender').html('Gender: ' + value.Gender);
+            });
+            
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        };
+    }
 }
 
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(addClickHandlersToButtons)
+// -----------------------------------------------------
+
+// -------------contact button--------------------------
 function myFunction() {
     contacts.on('value', function(snapshot) {
         var contacts = snapshot.val();
         alert('Phone: ' + contacts.Phone + '\n' + 'Email: ' + contacts.Email);
     });
 }
-
-
+// -----------------------------------------------------
 
 
